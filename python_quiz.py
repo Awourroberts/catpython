32405a9fad2410430c07985060d0556a20c61639
Awourrobberrt <sophierobberrt@gmail.com>
2021-06-17 10:48:24 +0300

    second commit

---
+++ python_quiz.py
@@ -0,0 +1,37 @@
+ x = [100,110,120,130,140,150], 
+ y = (50,30,40,60,100,200),
+    print list(5),
+function divisible_by_three():
+    range=(1%==3)
+    for range in a:
+    print(n)
+list x = [[1,2],[3,4],[5,6]],
+tuple(x):
+    print(x)
+function smallest():
+    smallest([3,6,8,2,4,1,5,7]),
+    range=(s%==0)
+    print(s%==l)
+x = ['a','b','a','e','d','b','c','e','f','g','h']
+    key.append('a','b','a','e','d','b','c','e','f','g','h')
+    x.add():
+    print(x)
+function divisible_by_seven ():
+for num in j:
+            rang=(100,200)
+            print(all)
+function students():
+students = [{"age": 19, "name": "Eunice"}, 
+           {"age":21, "name": "Agnes"},
+           {"age": 18, "name": "Teresa"}, 
+           {"age": 22, "name": "Asha"}],
+        student1=("age","name") 
+        student2=("age","name")
+        student3=("age","name")
+        student4=("age","name")          
+                print(student["name"])
+                print (student["age"])
+ function greet():
+        return f" Hello $name you were born in the $ year2002"
+                    
+            
